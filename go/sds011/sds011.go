dbb93430374a0e131344147ab3401d1de67debc0
Ric Szopa <ryszard.szopa@gmail.com>
2017-02-24 12:15:11 +0100

    Added Lincense.

--- go/sds011/sds011.go
+++ go/sds011/sds011.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Ryszard Szopa <ryszard.szopa@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package sds011 implements a library to read the protocol of SDS011,
 // an air quality sensor than can work with Raspberry Pi.
 package sds011
